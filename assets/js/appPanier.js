329304986708944a9a1e9d808a484698ef66606d
Yams02 <dzaidzai@hotmail.fr>
2018-10-31 21:55:24 +0100

    DiscountCars

--- assets/js/appPanier.js
+++ assets/js/appPanier.js
@@ -20,15 +20,15 @@ angular.module('myApp')
   };
   /*appPanier*/
   $scope.inventory = [
-    { id :  1, category : "audi", description : "small water bottle",   price :   2.99, qty : 1, image: "assets/img/Audi-a3.jpg" },
-    { id :  2, category : "citroen", description : "large water bottle",   price :   2.99, qty : 1, image: "assets/img/citroen-2cv.jpg", onSale : true },
-    { id :  3, category : "citroen",   description : "small flashlight",     price :   6.99, qty : 1, image: "assets/img/citroen-c3.jpg" },
-    { id :  4, category : "peugeot",   description : "big flashlight",       price :  12.99, qty : 1, image: "assets/img/peugeot-205.jpg" },
-    { id :  5, category : "peugeot",        description : "small stove",          price :  29.99, qty : 1, image: "assets/img/peugeot-police.jpg" },
-    { id :  6, category : "renault",        description : "big stove",            price :  29.99, qty : 1, image: "assets/img/renault-5.jpg" },
-    { id :  7, category : "renault", description : "simple sleeping bag",  price :  49.99, qty : 1, image: "assets/img/renault-clio.jpg" },
-    { id :  8, category : "renault", description : "deluxe sleeping bag",  price :  79.99, qty : 1, image: "assets/img/renault-scenic.jpg" },
-    { id :  9, category : "voiture enfant",         description : "1-person tent",        price : 119.99, qty : 1, image: "assets/img/voiture-enfant.jpg"}
+    { id :  1, category : "audi", description : "Audi A3 Diesel", price : 2.99, qty : 1, image: "assets/img/Audi-a3.jpg" },
+    { id :  2, category : "citroen", description : "Citroen 2ch", price : 2.99, qty : 1, image: "assets/img/citroen-2cv.jpg" },
+    { id :  3, category : "citroen", description : "Citroen C3 cactus", price :   6.99, qty : 1, image: "assets/img/citroen-c3.jpg" },
+    { id :  4, category : "peugeot", description : "Peugeot 205", price : 12.99, qty : 1, image: "assets/img/peugeot-205.jpg" },
+    { id :  5, category : "peugeot", description : "Peugeot 405", price : 29.99, qty : 1, image: "assets/img/peugeot-police.jpg" },
+    { id :  6, category : "renault", description : "Renault 5", price : 29.99, qty : 1, image: "assets/img/renault-5.jpg" },
+    { id :  7, category : "renault", description : "Renault Clio", price : 49.99, qty : 1, image: "assets/img/renault-clio.jpg" },
+    { id :  8, category : "renault", description : "Renault Scenic", price : 79.99, qty : 1, image: "assets/img/renault-scenic.jpg" },
+    { id :  9, category : "voiture enfant", description : "Collection", price : 119.99, qty : 1, image: "assets/img/voiture-enfant.jpg"}
   ];
 
   $scope.cart = [];
@@ -68,5 +68,4 @@ angular.module('myApp')
     var index = $scope.cart.indexOf(item);
     $scope.cart.splice(index, 1);
   };
-
 });
