0a0976f258be16ded97e9ea7281acc0094beef1e
Yams02 <dzaidzai@hotmail.fr>
2018-11-01 19:26:20 +0100

    Discount Cars

--- assets/js/appPanier.js
+++ assets/js/appPanier.js
@@ -1,20 +1,25 @@
 angular.module('myApp', ['ngAnimate']);
-
+/*module myApp*/
 angular.module('myApp')
+/*controller myApp */
 .controller('myController', ['$scope', '$window', function ($scope, $window) {
+  /*function alert s'identifier*/
   $scope.greeting = '';
   $scope.doGreeting = function(greeting) {
     $window.alert(greeting);
   };
-  $scope.contact = 'Pour plus de renseignement contatcter'+'\n'+'\n'+'Service clients : +330911821802';
+  /*function alert contact*/
+  $scope.contact = 'Pour plus de renseignement contatcter'+'\n'+'\n'+'Service clients :'+ '+330911821802';
   $scope.contactA = function(contact) {
     $window.alert(contact);
   };
+  /*function formulaire inscription*/
   $scope.master = {};
   $scope.regex = '\\d+';
   $scope.update = function(user) {
     $scope.master = angular.copy(user);
   };
+  /*function soumettre formulaire inscription*/
   $scope.message = ' ';
   $scope.soumission = function (valid) {
     if(valid) {
@@ -26,7 +31,7 @@ angular.module('myApp')
       $scope.message = 'Désolé mais il y a des données non valides !';
     }
   };
-  /*appPanier*/
+  /*appPanier Json*/
   $scope.inventory = [
     { id :  1, category : "audi", description : "Audi A3 Diesel", price : 2.99, qty : 1, image: "assets/img/Audi-a3.jpg" },
     { id :  2, category : "citroen", description : "Citroen 2ch", price : 2.99, qty : 1, image: "assets/img/citroen-2cv.jpg" },
@@ -38,19 +43,18 @@ angular.module('myApp')
     { id :  8, category : "renault", description : "Renault Scenic", price : 79.99, qty : 1, image: "assets/img/renault-scenic.jpg" },
     { id :  9, category : "voiture enfant", description : "Collection", price : 119.99, qty : 1, image: "assets/img/voiture-enfant.jpg"}
   ];
-
+  /*fucntion cartArticle = Json = item*/
   $scope.cart = [];
-
   var findItemById = function(items, id) {
     return _.find(items, function(item) {
       return item.id === id;
     });
   };
-
+  /*function qty x price*/
   $scope.getCost = function(item) {
     return item.qty * item.price;
   };
-
+  /*function ajout item panier*/
   $scope.addItem = function(itemToAdd) {
     var found = findItemById($scope.cart, itemToAdd.id);
     if (found) {
@@ -58,22 +62,22 @@ angular.module('myApp')
     }
     else {
       $scope.cart.push(angular.copy(itemToAdd));}
-  };
-
-  $scope.getTotal = function() {
-    var total =  _.reduce($scope.cart, function(sum, item) {
-      return sum + $scope.getCost(item);
-    }, 0);
-    console.log('total: ' + total);
-    return total;
-  };
-
-  $scope.clearCart = function() {
-    $scope.cart.length = 0;
-  };
-
-  $scope.removeItem = function(item) {
-    var index = $scope.cart.indexOf(item);
-    $scope.cart.splice(index, 1);
-  };
-}]);
+    };
+    /*function calcul du panier*/
+    $scope.getTotal = function() {
+      var total =  _.reduce($scope.cart, function(sum, item) {
+        return sum + $scope.getCost(item);
+      }, 0);
+      console.log('total: ' + total);
+      return total;
+    };
+    /*function vider le panier*/
+    $scope.clearCart = function() {
+      $scope.cart.length = 0;
+    };
+    /*function supprimer item*/
+    $scope.removeItem = function(item) {
+      var index = $scope.cart.indexOf(item);
+      $scope.cart.splice(index, 1);
+    };
+  }]);
